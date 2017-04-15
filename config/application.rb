49ac6b3c5b7b56c9675a5e31e75e8113f0ce9540
guxiaobai <sikuan.gu@gmail.com>
2017-04-15 18:13:48 +0800

    enable uuid

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module App
   class Application < Rails::Application
     config.generators.assets = false
     config.generators.helper = false
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
