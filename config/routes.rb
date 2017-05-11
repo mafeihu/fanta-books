a6cb4064665cb59247e614f5a6e38fb6cf87f5d8
guxiaobai <sikuan.gu@gmail.com>
2017-05-11 14:37:56 +0800

    七牛图片上传

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   root 'welcome#index'
-
   resources :books
-
+  namespace :api do
+    get 'qiniu' => 'qiniu#index'
+  end
+  
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
