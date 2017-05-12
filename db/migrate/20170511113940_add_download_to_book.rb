6753ad740cd37304957d4a2a9dbfba275cb6acbc
guxiaobai <sikuan.gu@gmail.com>
2017-05-12 11:32:04 +0800

    添加下载链接

---
+++ db/migrate/20170511113940_add_download_to_book.rb
@@ -0,0 +1,4 @@
+class AddDownloadToBook < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415104654) do
+ActiveRecord::Schema.define(version: 20170511113940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
